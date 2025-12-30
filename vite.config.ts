0692cc8c36c67d55606141a9fd7a681cd57c49f6
regismorenopr <regismorenopr@gmail.com>
2025-12-30 11:19:28 -0300

    Set Vite base path to /studio

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+﻿import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -21,3 +21,5 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
+
